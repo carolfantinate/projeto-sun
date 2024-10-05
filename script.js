799226b0985d6bb463b66a8b141ec7545eb7f539
Carol Fantinate <carolfantinate@outlook.com>
2024-10-05 17:15:26 -0300

    finalização chat

--- script.js
+++ script.js
@@ -4,4 +4,13 @@ function carregarAba3(){
     document.getElementById("aba3").style.width = "25vw";
     document.getElementById("aba3").style.height = "100vh";
     document.getElementById("aba2").style.width = "50%";
+    document.getElementById("but2").style.display = "inline"
+}
+
+function voltar() {
+    document.getElementById("aba-inicial").innerHTML = "";
+
+    document.getElementById("aba2").style.width = "75%";
+
+    document.getElementById("aba3").style.display = "none";
 }
