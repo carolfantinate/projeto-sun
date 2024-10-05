cc6eb8c057df0557da387154c5318c772a114988
Carol Fantinate <carolfantinate@outlook.com>
2024-10-05 16:04:08 -0300

    Initial commit

---
+++ script.js
@@ -0,0 +1,7 @@
+function carregarAba3(){
+    document.getElementById("aba-inicial").innerHTML = "<iframe src='chat-3.html' frameborder='0' id='aba3'></iframe>";
+
+    document.getElementById("aba3").style.width = "25vw";
+    document.getElementById("aba3").style.height = "100vh";
+    document.getElementById("aba2").style.width = "50%";
+}
