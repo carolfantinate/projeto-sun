8fbbe1cb369a0f975c5ce257b20c60ecf0d2209d
Carol Fantinate <carolfantinate@outlook.com>
2024-11-03 12:28:25 -0300

    aba de atendimento

--- calendar.js
+++ calendar.js
@@ -1,29 +1,31 @@
+/*----------variaveis globais-----------*/
+
 const monthNames = [
     "Janeiro", "Fevereiro", "Março", "Abril", "Maio", "Junho",
     "Julho", "Agosto", "Setembro", "Outubro", "Novembro", "Dezembro"
 ];
 
-let currentDate = new Date();
+let currentDate = new Date(); //data atual
 
 function loadCalendar() {
     const month = currentDate.getMonth();
     const year = currentDate.getFullYear();
 
-    document.getElementById("month-year").textContent = `${monthNames[month]} ${year}`;
-    const firstDay = new Date(year, month, 1).getDay();
-    const daysInMonth = new Date(year, month + 1, 0).getDate();
-    const daysInPrevMonth = new Date(year, month, 0).getDate();
+    document.getElementById("month-year").textContent = `${monthNames[month]} ${year}`;  //mês e ano atuais
+    const firstDay = new Date(year, month, 1).getDay(); //primeiro dia do mês
+    const daysInMonth = new Date(year, month + 1, 0).getDate(); //total de dias do mês
+    const daysInPrevMonth = new Date(year, month, 0).getDate(); //dias do mês anterior
 
     const calendarBody = document.getElementById("calendar-body");
-    calendarBody.innerHTML = "";
+    calendarBody.innerHTML = ""; 
 
     let date = 1;
 
     for (let i = 0; i < 6; i++) {
-        const row = document.createElement("tr");
+        const row = document.createElement("tr"); //criar as linhas (semanas)
 
         for (let j = 0; j < 7; j++) {
-            const cell = document.createElement("td");
+            const cell = document.createElement("td"); //criar colunas (dia da semana)
 
             if (i === 0 && j < firstDay) {
                 cell.textContent = daysInPrevMonth - firstDay + j + 1;
