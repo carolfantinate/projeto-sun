0d98b5e834122c2d94087a3ea4b1b321f3c51e7d
Carol Fantinate <carolfantinate@outlook.com>
2024-12-01 12:13:16 -0300

    estilização e js - cadastro esp1

--- cadastros/cad-esp1.js
+++ cadastros/cad-esp1.js
@@ -1,32 +1,33 @@
 const form = document.getElementById('form')
 const nome = document.getElementById('nome')
+const email = document.getElementById('email')
+const especialidade = document.getElementById('esp')
 const senha = document.getElementById('senha')
-const passwordConfirmation = document.getElementById('c-senha')
 
 form.addEventListener('submit', (e) => {
     e.preventDefault();
 
     checkInputs();
-});
+})
 
 function checkInputs() {
     const nomeValue = nome.value
     const emailValue = email.value
+    const especialidadeValue = especialidade.value
     const senhaValue = senha.value
-    const passwordConfirmationValue = passwordConfirmation.value
 
     //verificando o nome
     if (nomeValue === "") {
-        setErrorFor(nome, 'O nome de usuário é obrigatório.')
+        setErrorFor(nome, 'O nome completo é obrigatório.')
     } else {
         setSuccessFor(nome)
     }
 
-    //verificando o email
+    //verificando email
     if (emailValue === "") {
         setErrorFor(email, 'O email é obrigatório.')
-    } else if (!checkEmail(emailValue)) {
-        setErrorFor(email, 'Por favor, insira um email válido.')
+    } else if (!checkEmail(emailValue)){
+        setErrorFor(email, "Por favor, insira um email válido.")
     } else {
         setSuccessFor(email)
     }
@@ -40,13 +41,10 @@ function checkInputs() {
         setSuccessFor(senha)
     }
 
-    //confirmação de senha 
-    if (passwordConfirmationValue === "") {
-        setErrorFor(passwordConfirmation, 'A confirmação de senha é obrigatória.')
-    } else if (passwordConfirmationValue !== senhaValue) {
-        setErrorFor(passwordConfirmation, 'As senhas não conferem.')
+    if (especialidadeValue === ""){
+        setErrorFor(especialidade, 'A especialidade deve ser preenchida.')
     } else {
-        setSuccessFor(passwordConfirmation)
+        setSuccessFor(especialidade)
     }
 
     //validando o formulário
@@ -58,29 +56,25 @@ function checkInputs() {
 
     if (formIsValid) {
         sessionStorage.setItem('formularioConcluido', 'true');
-        window.location.href = 'cad-user2.html';
+        window.location.href = 'cad-esp2.html'
     }
+
 }
 
 function setErrorFor(input, message) {
     const formControl = input.parentElement;
     const small = formControl.querySelector('small')
 
-    //add a mensagem de erro
     small.innerText = message;
 
-    //add a classe de erro
     formControl.className = 'form-control error'
-
 }
 
 function setSuccessFor(input) {
     const formControl = input.parentElement;
 
-    //add a classe de sucesso
     formControl.className = 'form-control success'
 }
-
 
 function checkEmail(email) {
     return /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/.test(
